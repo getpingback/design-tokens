915b346d28f0659ceaf5f36662ac13185db8150e
roger067 <rosestolato.rogerio@gmail.com>
2025-08-27 10:42:46 -0300

    feat: export dark and light theme

--- config.js
+++ config.js
@@ -1,52 +1,58 @@
 import StyleDictionary from 'style-dictionary';
-// import { fileHeader, formattedVariables } from 'style-dictionary/utils';
-// import { propertyFormatNames } from 'style-dictionary/enums';
-import {
-  logBrokenReferenceLevels,
-  logVerbosityLevels,
-  logWarningLevels,
-} from 'style-dictionary/enums';
+import { fileHeader, formattedVariables } from 'style-dictionary/utils';
 import { register } from '@tokens-studio/sd-transforms';
 
 register(StyleDictionary, { excludeParentKeys: true });
 
-// StyleDictionary.registerFormat({
-//   name: 'css/variables-themed',
-//   format: async ({ dictionary, file, options }) => {
-//     const { outputReferences, theme } = options;
-//     const header = await fileHeader({ file });
-//     return (
-//       header +
-//       `.${theme} {\n` +
-//       formattedVariables({
-//         format: propertyFormatNames.css,
-//         dictionary,
-//         outputReferences,
-//       }) +
-//       '\n}\n'
-//     );
-//   },
-// });
+const filterTheme = (contents, modeToDelete) => {
+  const tokens = JSON.parse(contents);
+  if (tokens.hasOwnProperty(modeToDelete)) {
+    delete tokens[modeToDelete];
+  }
 
-// StyleDictionary.registerParser({
-//   name: 'token-parser',
-//   pattern: /\.json$/,
-//   parser: ({ contents }) => {
-//     const tokens = JSON.parse(contents);
-//     return tokens;
-//   },
-// });
+  return tokens;
+};
 
-const sd = new StyleDictionary({
-  source: ['tokens.json'],
-  log: {
-    warnings: logWarningLevels.warn,
-    verbosity: logVerbosityLevels.default,
-    errors: {
-      brokenReferences: logBrokenReferenceLevels.throw,
-    },
+StyleDictionary.registerFormat({
+  name: 'css/variables-themed',
+  format: async ({ dictionary, file, options }) => {
+    const { outputReferences, theme } = options;
+    const header = await fileHeader({ file });
+    return (
+      header +
+      `.${theme} {\n` +
+      formattedVariables({
+        format: 'css',
+        dictionary,
+        outputReferences,
+      }) +
+      '\n}\n'
+    );
   },
+});
+
+StyleDictionary.registerParser({
+  name: 'light-parser',
+  pattern: /\.json$/,
+  parser: ({ contents }) => {
+    const tokens = filterTheme(contents, 'Dark Mode');
+    return tokens;
+  },
+});
+
+StyleDictionary.registerParser({
+  name: 'dark-parser',
+  pattern: /\.json$/,
+  parser: ({ contents }) => {
+    const tokens = filterTheme(contents, 'Light Mode');
+    return tokens;
+  },
+});
+
+const lightSD = new StyleDictionary({
+  source: ['tokens.json'],
   preprocessors: ['tokens-studio'],
+  parsers: ['light-parser'],
   platforms: {
     css: {
       transformGroup: 'tokens-studio',
@@ -54,7 +60,7 @@ const sd = new StyleDictionary({
       buildPath: 'dist/css/',
       files: [
         {
-          destination: 'globals.css',
+          destination: 'light.css',
           format: 'css/variables',
         },
       ],
@@ -65,7 +71,7 @@ const sd = new StyleDictionary({
       transforms: ['name/constant'],
       files: [
         {
-          destination: 'variables.js',
+          destination: 'light.js',
           format: 'javascript/es6',
         },
       ],
@@ -73,5 +79,40 @@ const sd = new StyleDictionary({
   },
 });
 
-await sd.cleanAllPlatforms();
-await sd.buildAllPlatforms();
+const darkSD = new StyleDictionary({
+  source: ['tokens.json'],
+  preprocessors: ['tokens-studio'],
+  parsers: ['dark-parser'],
+  platforms: {
+    css: {
+      transformGroup: 'tokens-studio',
+      transforms: ['name/kebab'],
+      buildPath: 'dist/css/',
+      files: [
+        {
+          destination: 'dark.css',
+          format: 'css/variables',
+        },
+      ],
+    },
+    js: {
+      transformGroup: 'tokens-studio',
+      buildPath: 'dist/js/',
+      transforms: ['name/constant'],
+      files: [
+        {
+          destination: 'dark.js',
+          format: 'javascript/es6',
+        },
+      ],
+    },
+  },
+});
+
+async function cleanAndBuild(config) {
+  const sd = new StyleDictionary(config);
+  await sd.cleanAllPlatforms();
+  await sd.buildAllPlatforms();
+}
+
+await Promise.all([cleanAndBuild(lightSD), cleanAndBuild(darkSD)]);
